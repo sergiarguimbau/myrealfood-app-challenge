e2a57ea37d96c7d65c9d15afa4e71485c3148ec3
Igresator <sergiarguimbau@gmail.com>
2021-01-21 21:05:55 +0100

    Rename Android project to "MyRealFood"

--- android/app/src/main/java/com/myrealfood/appchallenge/MainActivity.java
+++ android/app/src/main/java/com/myrealfood/appchallenge/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.myrealfood.appchallenge;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeStarter";
+        return "MyRealFood";
     }
 }
--- android/app/src/main/java/com/myrealfood/appchallenge/MainApplication.java
+++ android/app/src/main/java/com/myrealfood/appchallenge/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativestarter;
+package com.myrealfood.appchallenge;
 
 import android.app.Application;
 import android.content.Context;
